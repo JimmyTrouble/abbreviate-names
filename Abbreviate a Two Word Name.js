b90d8490394ef6bcf79ec4e7125fb5f6435ffbef
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2022-10-16 19:02:19 +1100

    Add files via upload

---
+++ Abbreviate a Two Word Name.js
@@ -0,0 +1,23 @@
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+abbrevName("Sam Harris");
+
+function abbrevName(name){
+    
+    let first = name.charAt(0);
+    let second = name.charAt(name.indexOf(" ") + 1);
+    let inetials = first.concat(".",second);
+    
+}
+
+
+
+// let name = "Sam Harris";
+
+// console.log(name.indexOf(" "));
+// let first = name.charAt(0);
+// let second = name.charAt(name.indexOf(" ") + 1);
+// let inetials = first.concat(".",second);
+// console.log(inetials);
